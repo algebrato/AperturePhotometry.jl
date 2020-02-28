2cf5ea65283368883e0435a42026acd025261509
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-28 08:04:47 +0000

    build based on 6e313b2

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AperturePhotometry.jl-1","page":"Home","title":"AperturePhotometry.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [AperturePhotometry]","category":"page"}]
+}
